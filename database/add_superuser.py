a2e987e905c8de3ddcf1cac8b29c5fd2418e671b
Shahed <shahed_hridoy@outlook.com>
2025-02-18 11:39:15 +0600

    hide password

--- database/add_superuser.py
+++ database/add_superuser.py
@@ -1,6 +1,7 @@
 # Add the parent directory to the sys.path
 import sys
 import os
+import getpass
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -10,9 +11,9 @@ from database.user import add_user # noqa
 def create_superuser():
     """ Add a superuser to the database """
     username = input("Enter username: ")
-    password = input("Enter password: ")
+    password = getpass.getpass("Enter password: ")
     if add_user(username, password, is_superuser=True):
-        print(f"Superuser {username} created successfully!")
+        print(f"Superuser '{username}' created successfully!")
     else:
         print("Error adding superuser!")
 
